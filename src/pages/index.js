7dd9169a142bda65510fb06a00a88010645342ba
BobTabrizi <btabrizi@ucsd.edu>
2021-07-06 13:03:03 -0700

    Oops

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,34 +5,6 @@ import { About } from "../components/About.js";
 import { Projects } from "../components/Projects.js";
 import { Contact } from "../components/Contact.js";
 export default function Home() {
-  const section1Ref = useRef();
-  const section2Ref = useRef();
-  const section3Ref = useRef();
-  const section4Ref = useRef();
-
-  const navHeader = [
-    {
-      headerTitle: "Section 1",
-      headerRef: section1Ref,
-      headerID: "section1",
-    },
-    {
-      headerTitle: "Section 2",
-      headerRef: section2Ref,
-      headerID: "section2",
-    },
-    {
-      headerTitle: "Section 3",
-      headerRef: section3Ref,
-      headerID: "section3",
-    },
-    {
-      headerTitle: "Section 4",
-      headerRef: section4Ref,
-      headerID: "section4",
-    },
-  ];
-
   return (
     <>
       <Head>
@@ -51,7 +23,7 @@ export default function Home() {
         />
       </Head>
 
-      <section id="home" ref={section1Ref}>
+      <section id="home">
         <Canvas />
       </section>
 
